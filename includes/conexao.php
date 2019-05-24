f8307178d1b1e08ee93f173d2a54d9b8034a44cf
Diego Oliveira <diego.oliveira@gmail.com>
2019-05-23 22:57:49 -0300

    Connection string

--- includes/conexao.php
+++ includes/conexao.php
@@ -10,7 +10,7 @@ class Conexao {
  
     public static function getInstance() {
         if (!isset(self::$instance)) {
-            self::$instance = new PDO( 'mysql:host=db-mxc.mysql.database.azure.com:3306;dbname=db-mxc', 'db-mxc@db-mxc', '@lph@23010@', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            self::$instance = new PDO( 'mysql:host=db-mxc.mysql.database.azure.com;dbname=mpc', 'db-mxc@db-mxc', '@lph@23010@', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
         }
